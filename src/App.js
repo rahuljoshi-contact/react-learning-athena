213d77b68fd83573aa69fb30a636b1eade7c5f07
Rahul Joshi <rahjoshi@athenahealth.com>
2019-11-06 15:12:52 -0500

    usestate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   const h1Style = {
@@ -6,17 +6,36 @@ function App() {
     fontWeight: "bold"
   };
 
-  const users = [
+  const [users, handleUsers] = useState([
     { id: 1, name: "Cory", email: "Cory@c.com" },
-    { id: 3, name: "Megan", email: "Megan@m.com" },
+    { id: 2, name: "Megan", email: "Megan@m.com" },
     { id: 3, name: "Tammy", email: "Tammy@t.com" },
     { id: 4, name: "Jack", email: "Jack@j.com" }
-  ];
+  ]);
 
+  //const users = ;
+
+  function handleDelete(id) {
+    //debugger;
+    const newUsers = users.filter(user => user.id !== id);
+    handleUsers(newUsers);
+  }
   //debugger;
   return (
     <>
-      <h1 className="header">hello App</h1>
+      <h1 className="header">User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {/* delay execution using the arrow function */}
+            <button name="Delete" onClick={() => handleDelete(user.id)}>
+              Delete
+            </button>
+            {user.name}
+          </li>
+        ))}
+      </ul>
+
       <label htmlFor="firstName" style={h1Style}>
         First Name:{" "}
       </label>
